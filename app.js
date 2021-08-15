46166a56837505c521c502c212bdaf86c10e14e7
Saranya3 <saranya.u03@gmail.com>
2021-08-15 12:57:05 +0530

    Latest commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-var _ = require('lodash');
+const mongoose = require('mongoose');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -11,71 +11,70 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
-var posts=[];
-
-
 app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-//Home route
-app.get("/",function(req,res){
-    res.render("home.ejs",{homeStartingContent:homeStartingContent,posts:posts});
-});
+mongoose.connect("mongodb://localhost:27017/blogDB", {useNewUrlParser: true});
 
-//About route
-app.get("/about",function(req,res){
-  res.render("about.ejs",{aboutContent:aboutContent});
-});
+const postSchema = {
+  title: String,
+  content: String
+};
 
-//Contact route
-app.get("/contact",function(req,res){
-  res.render("contact.ejs",{contactContent:contactContent});
-});
+const Post = mongoose.model("Post", postSchema);
 
-//Compose route
-app.get("/compose",function(req,res){
-  res.render("compose.ejs");
-});
+app.get("/", function(req, res){
 
-app.post("/compose",function(req,res){
-  let post={
-    title: req.body.postTitle,
-    body: req.body.postBody
-  };
-  posts.push(post);
-  
-  res.redirect("/");
-});
-
-app.get("/posts/:postName",function(req,res){
-  // Format URL
-  var reqUrl=_.kebabCase(_.lowerCase(req.params.postName));
- 
-
-  posts.forEach(function(post){
-    // Format URL
-    var storedUrl= _.kebabCase(_.lowerCase(post.title));
-  
-    if(reqUrl === storedUrl){
-      res.render("post",{title:post.title,body:post.body});
-    }
-    else{
-      console.log("Not equal");
-    }
-
+  Post.find({}, function(err, posts){
+    res.render("home", {
+      startingContent: homeStartingContent,
+      posts: posts
+      });
   });
-  
 });
 
-app.listen(3000,function(){
-  console.log("Check out Port 3000!!");
+app.get("/compose", function(req, res){
+  res.render("compose");
+});
+
+app.post("/compose", function(req, res){
+  const post = new Post({
+    title: req.body.postTitle,
+    content: req.body.postBody
+  });
+
+
+  post.save(function(err){
+    if (!err){
+        res.redirect("/");
+    }
+  });
+});
+
+app.get("/posts/:postId", function(req, res){
+
+const requestedPostId = req.params.postId;
+
+  Post.findOne({_id: requestedPostId}, function(err, post){
+    res.render("post", {
+      title: post.title,
+      content: post.content
+    });
+  });
+
+});
+
+app.get("/about", function(req, res){
+  res.render("about", {aboutContent: aboutContent});
+});
+
+app.get("/contact", function(req, res){
+  res.render("contact", {contactContent: contactContent});
 });
 
 
-
-
-
-
-
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
+});
